6cfa42da8c01601817fe31fbc0161a47abcadd81
Jay <you@example.com>
2024-07-31 19:30:42 +0500

    contacts - mobile - start

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,13 @@
 import '../css/reset.css';
 import '../css/main.css';
 import '../css/contacts.css';
+import Inputmask from "inputmask";
+
+// phone mask 
+document.addEventListener("DOMContentLoaded", function() {
+    var phoneInputs = document.querySelectorAll('input[type="tel"]');
+    var im = new Inputmask("+7 (999) 999-99-99");
+    phoneInputs.forEach(function(input) {
+        im.mask(input);
+    });
+});
