aa3b3e7fdacc1ad2d494814b80b1cbba9ebd7035
Jay <you@example.com>
2024-08-07 19:33:14 +0500

    popups, vacancy-adaptive

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let recallButton = document.querySelector('#recall-button');
+    let popup = document.querySelector('.callpack-popup');
+    let overlay = document.querySelector('.overlay');
+    let closePopup = document.querySelector('.closebutton');
+
+    recallButton.addEventListener('click', function() {
+        popup.classList.add('show');
+        overlay.classList.add('show');
+    });
+
+    closePopup.addEventListener('click', function() {
+        popup.classList.remove('show');
+        overlay.classList.remove('show');
+    });
+
+    overlay.addEventListener('click', function() {
+        popup.classList.remove('show');
+        overlay.classList.remove('show');
+    });
+});
